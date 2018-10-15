6914e16a42b1c15b82cdb42ab8fb97f10afaa2d9
Ginnynjau <njau.virginiah@gmail.com>
2018-10-15 20:08:02 +0300

    Store Manager - Products page
    The products page allows a user to:
      - Add new products
      - Edit a product's information
      - Delete a product
      - Add a new product category
      - Modify a product category
      - Delete a product category
      - View all the products in the inventory
    
    [Delivers #161110081]
    [Delivers #161110081]
    
     This commit also:
    	-  modified the CSS and Javascript files
    	- fixed the login and dashboard pages. [Fixed #161108764]

---
+++ ui/main.js
@@ -0,0 +1,59 @@
+function openProductTab(evt, productTab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(productTab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+function openUsersTab(evt, usersTab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(usersTab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+function openSalesTab(evt, salesTab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(salesTab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openReportsTab(evt, reportsTab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(reportsTab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
